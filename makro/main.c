7a1bf9155327d912433ba66f0f5017ccfab34261
Carsten Schmidt <jazzschmidt@icloud.com>
2020-04-02 19:15:46 +0200

    Adds makro stuff

---
+++ makro/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define is =
+#define dude ;
+#define just
+#define somehow
+#define magically
+#define enter printf("\n");
+#define show(a) printf("%2i", a)
+#define wtf return 0;
+
+int main(int argc, char const *argv[]) {
+  int zahl is somehow 10;
+
+  // Slide to zero
+  while(zahl --> 0) {
+    just show(zahl) dude
+    magically enter
+  }
+
+  dude wtf
+}
