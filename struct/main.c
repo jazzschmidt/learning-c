613ab4dd891e0bdd0bb9ff80311b8593710abcbb
Carsten Schmidt <jazzschmidt@icloud.com>
2020-04-14 18:17:22 +0200

    Creates a structure and switches its values

---
+++ struct/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+
+int main(int argc, char const *argv[]) {
+  struct Point p1 = createPoint(20, 40);
+  struct Point p2 = createPoint(30, 15);
+
+  switchPointXY(&p1);
+  switchPointXY(&p2);
+
+  validatePointXY(&p1, 40, 20);
+  validatePointXY(&p2, 15, 30);
+
+  return 0;
+}
+
+struct Point createPoint(int x, int y) {
+  return (struct Point){ .x=x, .y=y };
+}
+
+void switchPointXY(struct Point* point) {
+  int xTmp = point->x;
+  point->x = point->y;
+  point->y = xTmp;
+}
+
+void validatePointXY(struct Point* point, int x, int y) {
+  if(point->x != x || point->y != y) {
+    exit(1);
+  }
+}
---
+++ struct/struct.h
@@ -0,0 +1,10 @@
+
+struct Point {
+  int x, y;
+};
+
+struct Point createPoint(int, int);
+
+void switchPointXY(struct Point*);
+
+void validatePointXY(struct Point*, int, int);
