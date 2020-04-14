6733ab9be22e714f034ea198b068a1508664e4ee
Carsten Schmidt <jazzschmidt@icloud.com>
2020-04-14 20:25:33 +0200

    Reallocates memory istead of allocating it

--- list/main.c
+++ list/main.c
@@ -32,7 +32,7 @@ intlist il_create() {
 }
 
 long il_push(intlist* list, int value) {
-  list->elements = malloc(++list->size * sizeof(int));
+  list->elements = realloc(list->elements, ++list->size * sizeof(int));
   return list->size;
 }
 
@@ -41,5 +41,5 @@ int il_get(intlist* list, long index) {
 }
 
 void il_pop(intlist* list) {
-
+  
 }
