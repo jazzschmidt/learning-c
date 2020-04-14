be9c9a58757fc3c109d970f9b06f41da31d50dff
Carsten Schmidt <jazzschmidt@icloud.com>
2020-04-14 20:49:41 +0200

    Assigns and retrieves values

--- list/main.c
+++ list/main.c
@@ -2,27 +2,35 @@
 #include <stdlib.h>
 #include "list.h"
 
-void assert(char* msg, char expression) {
+void assert(char* msg, long actual, char expression) {
   static int assert_nr = 0;
   ++assert_nr;
 
   if(!expression) {
-    fprintf(stderr, "[ERROR] Assertion %d failed: %s\n", assert_nr, msg);
+    fprintf(stderr, "[ERROR] Assertion %d failed\n", assert_nr);
+    fprintf(stderr, "[ERROR] Expected: %s\n", msg);
+    fprintf(stderr, "[ERROR] Actual:   %ld\n", actual);
   }
 }
 
 int main(int argc, char const *argv[]) {
   intlist list = il_create();
-  assert("Size is 0", list.size == 0);
+  assert("Size is 0", list.size, list.size == 0);
 
   il_push(&list, 123);
-  assert("Size is 1", list.size == 1);
+  assert("Size is 1", list.size, list.size == 1);
+
+  int first = il_get(&list, 0);
+  assert("First element is 123", first, first == 123);
 
   il_push(&list, 456);
-  assert("Size is 2", list.size == 2);
+  assert("Size is 2", list.size, list.size == 2);
+
+  int last = il_get(&list, 1);
+  assert("Last element is 456", last, last == 456);
 
   il_pop(&list);
-  assert("Size is 1", list.size == 1);
+  assert("Size is 1", list.size, list.size == 1);
 
   return 0;
 }
@@ -33,11 +41,17 @@ intlist il_create() {
 
 long il_push(intlist* list, int value) {
   list->elements = realloc(list->elements, ++list->size * sizeof(int));
+  int *ptr = list->elements + (list->size-1);
+  *ptr = value;
+
   return list->size;
 }
 
 int il_get(intlist* list, long index) {
-  return 0;
+  int *ptr = list->elements;
+  ptr += index;
+
+  return *ptr;
 }
 
 void il_pop(intlist* list) {
